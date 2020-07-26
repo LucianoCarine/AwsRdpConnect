0173a7f5b4b1ddce4cfaca0de77e2e71b701ed5a
mwduncan2018 <47758549+mwduncan2018@users.noreply.github.com>
2020-07-26 12:01:04 -0400

    Create setup-rhel8-xrdp.sh

---
+++ setup-rhel8-xrdp.sh
@@ -0,0 +1,12 @@
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-8.noarch.rpm 
+sudo yum install -y tigervnc-server xrdp
+sudo systemctl start xrdp
+sudo netstat -antup | grep xrdp
+sudo system enable xrdp
+sudo yum install firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=3389/tcp
+sudo firewall-cmd --reload
+sudo passwd root
+
+echo "this still has bugs... figure it out later"
