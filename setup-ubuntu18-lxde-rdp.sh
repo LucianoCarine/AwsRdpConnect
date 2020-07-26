e493974d53848c5aa0730e37eb7ca456d39740d8
mwduncan2018 <47758549+mwduncan2018@users.noreply.github.com>
2020-07-26 12:02:37 -0400

    Update setup-ubuntu18-lxde-rdp.sh

--- setup-ubuntu18-lxde-rdp.sh
+++ setup-ubuntu18-lxde-rdp.sh
@@ -4,11 +4,19 @@ sudo apt-get install xrdp -y
 
 echo "========================================="
 echo "========================================="
-echo "Set a password with this command"
-echo "    sudo passwd ubuntu"
+echo "Set a password to login with RDP"
+echo "========================================="
+echo "========================================="
+sudo passwd ubuntu
+
+echo " "
+echo " "
+echo "========================================="
+echo "========================================="
 echo "The AWS Security Group must have an Inbound Rule of 'RDP - TCP - 3389 - 0.0.0.0/0'"
 echo "Start Windows Remote Desktop Protocol"
 echo "Connect to '<IPv4_Public_IP>:3389'"
+echo "Login with usernaame 'ubuntu'"
 echo "Enter your password"
 echo "Good to go!"
 echo "========================================="
